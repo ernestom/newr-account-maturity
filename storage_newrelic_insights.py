d9c6c13ba25956d2dbec5bffd04c1aa024ea707d
Paulo Monteiro <pauloesquilo@gmail.com>
2019-04-19 11:57:44 -0700

    add retries

--- storage_newrelic_insights.py
+++ storage_newrelic_insights.py
@@ -2,6 +2,8 @@ import json
 import os
 import requests
 
+MAX_RETRIES = 5 # max number of requests before giving up
+
 class StorageNewRelicInsights():
 
     INSIGHTS_MAX_EVENTS = 1000
@@ -19,7 +21,7 @@ class StorageNewRelicInsights():
     def get_accounts(self, name):
         return []
         
-    def dump_metrics(self, name, data=[], metadata={}):
+    def dump_metrics(self, name, data=[], metadata={}, max_retries=MAX_RETRIES):
         if type(data) == list:
             if len(metadata) > 0:
                 for row in data:
@@ -27,11 +29,20 @@ class StorageNewRelicInsights():
                     row.update(metadata)
             for i in range(0, len(data), StorageNewRelicInsights.INSIGHTS_MAX_EVENTS):
                 data_chunk = data[i:i+StorageNewRelicInsights.INSIGHTS_MAX_EVENTS]
-                requests.post(
-                    self.url,
-                    data=json.dumps(data_chunk),
-                    headers=self.headers
-                )
+                succeeded = False
+                count_retries = 0
+                while not succeeded and count_retries < max_retries:
+                    try:
+                        count_retries += 1
+                        response = requests.post(
+                            self.url,
+                            data=json.dumps(data_chunk),
+                            headers=self.headers
+                        )
+                        succeeded = (response.status_code == requests.codes.ok)
+                    except:
+                        succeeded = False
+                
 
     def flush_metrics(self, location):
         pass
