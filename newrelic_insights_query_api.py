fb9c451531f5dfc0ecb9a4fe447cd1357f6ff84e
Paulo Monteiro <pauloesquilo@gmail.com>
2019-05-11 09:10:23 -0700

    .

--- newrelic_insights_query_api.py
+++ newrelic_insights_query_api.py
@@ -3,26 +3,36 @@ import os
 import requests
 
 
-def o(obj, attr):
-    """ ensures Insights results are sound otherwise abort nicely """
-
-    if not attr in obj:
-        raise Exception(f'error: Cannot parse query {attr}')
-    return obj[attr]
+def get_empty(results, header, include={}):
+    """ returns an empty event list """
+    return []
 
 
-def get_results_names(contents):
+def get_results_header(contents):
     """ extracts results names from the contents attribute """
 
     names = []
     for content in contents:
-        if 'alias' in content:
-            names.append(content['alias'])
+        alias = content.get('alias', '')
+        if alias:
+            content = content['contents']
+        function = content['function']
+        attribute = content.get('attribute', '')
+        name = alias if alias else function + '_' + attribute if attribute else function
+        if function == 'percentile':
+            for threshold in content['thresholds']:
+                names.append(name + '_' + str(threshold)) 
+        elif function == 'histogram':
+            start = content['start']
+            size = content['bucketSize']
+            for i in range(0, content['bucketCount']):
+                end = start + size
+                bucket = '%05.2f' % start + '_' + '%05.2f' % end
+                names.append(name + '_' + bucket)
+                start = end
         else:
-            function = content.get('function', '')
-            attribute = content.get('attribute', '')
-            name = function + '_' + attribute if attribute else function
-            names.append(name)  
+            names.append(name)
+    
     return names
 
 
@@ -30,28 +40,35 @@ def get_facets_header(facet, header):
 
     data = {}
     if type(facet) is str:
-        offset = 1
         data.update({header[0]: facet})
     elif type(facet) is list:
-        offset = len(facet)
         data.update({k:v for k,v in list(zip(header, facet))})
+
     return data
 
-
-def get_none(results, header, include={}):
-    """ returns an empty event list """
-    return []
 
 
 def get_results(results, header, include={}, offset=0):
 
     row = {k:v for k,v in include.items()}
-    for index,result in enumerate(results):
-        row.update({header[offset+index]: list(result.values())[0]})
+    index = 0
+    for result in results:
+        if 'percentiles' in result:
+            for percentile in list(result['percentiles'].values()):
+                row.update({header[offset+index]: percentile})
+                index += 1
+        elif 'histogram' in result:
+            for histogram in result['histogram']:
+                row.update({header[offset+index]: histogram})
+                index += 1
+        else:          
+            row.update({header[offset+index]: list(result.values())[0]})
+            index += 1
+
     return row
 
 
-def get_facets(results, header, include={}):
+def get_facets(results, header, include={}, offset=0):
 
     data = []
     for result in results:
@@ -59,6 +76,7 @@ def get_facets(results, header, include={}):
         row = get_facets_header(result['name'], header)
         row.update(get_results(result['results'], header, include, offset=len(row)))
         data.append(row)
+
     return data
 
 
@@ -69,34 +87,35 @@ def get_timeseries(results, header, include={}, offset=0):
         # row = {k:v for k,v in include.items()}
         row = get_results(result['results'], header, include, offset)
         row.update({
-            'beginTimeSeconds': result['beginTimeSeconds'],
-            'endTimeSeconds': result['endTimeSeconds'],
             'inspectedCount': result['inspectedCount'],
+            'timewindow': result['endTimeSeconds'] - result['beginTimeSeconds'],
             'timestamp': result['endTimeSeconds']
         })
         data.append(row)
+
     return data
 
 
-def get_single(results, header, include={}):
+def get_single(results, header, include={}, offset=0):
     """ SELECT aggr1, aggr2, ... FROM ... """
 
     return [get_results(results, header, include=include)]
 
 
-def get_events(results, header, include={}):
+def get_events(results, header, include={}, offset=0):
     """ SELECT attr1, attr2, ... FROM ... """
     
     data = []
-    events = o(results[0], 'events')
+    events = results[0]['events']
     for event in events:
         row = {k:v for k,v in include.items()}
         row.update({k:v for k,v in event.items()})
         data.append(row)
+
     return data
 
 
-def get_facets_timeseries(results, header, include={}):
+def get_facets_timeseries(results, header, include={}, offset=0):
     """ SELECT aggr1(), aggr2(), ... FROM ... FACET attr1, attr2, ... TIMESERIES """
     
     data = []
@@ -110,23 +129,42 @@ def get_facets_timeseries(results, header, include={}):
                 result['timeSeries'], 
                 header, 
                 _include,
-                offset=len(row)
+                offset=offset
             )
         )
+
     return data
 
 
-def get_calc_with_compare_facet(results, header, include={}):
-    return []
+def get_calc_with_compare_facet(results, header, include={}, offset=0):
+
+    data = []
+    facets_current = results['current']['facets']
+    facets_previous = results['previous']['facets']
+    header_previous = [v if i < offset else v + '_compare' for i,v in enumerate(header)]
+    current = get_facets(facets_current, header, include, offset)
+    previous = get_facets(facets_previous, header_previous, {}, offset)
+    for curr,prev in list(zip(current, previous)):
+        curr.update(prev)
+        data.append(curr)
+
+    return data
 
 
-def get_calc_with_compare_timeseries(results, header, include={}):
+def get_calc_with_compare_timeseries(results, header, include={}, offset=0):
     return []
     
     
-def get_calc_with_compare(results, header, include={}):
-    return []
+def get_compare_with(results, header, include={}, offset=0):
+    """ SELECT aggr1(), aggr2(), ... FROM ... COMPARE WITH ... """
+    
+    current = results['current']['results']
+    previous = results['previous']['results']
+    header_previous = [v if i < offset else v + '_compare' for i,v in enumerate(header)]
+    row = get_results(current, header, include, offset)
+    row.update(get_results(previous, header_previous, {}, offset))
 
+    return [row]
 
 class NewRelicInsightsQueryAPI():
 
@@ -137,11 +175,13 @@ class NewRelicInsightsQueryAPI():
         
         if not new_relic_account_id:
             new_relic_account_id = os.getenv('NEW_RELIC_ACCOUNT_ID', '')
+
         if not new_relic_account_id:
             raise Exception('error: missing New Relic account id')
 
         if not query_api_key:
             query_api_key = os.getenv('NEW_RELIC_INSIGHTS_QUERY_API_KEY', '')
+
         if not query_api_key:
             raise Exception('error: missing New Relic Insights query API key')
 
@@ -153,7 +193,7 @@ class NewRelicInsightsQueryAPI():
         self.__max_retries = max_retries
 
     def query(self, nrql):
-        """ send a request to the Insights Query API """
+        """ request a JSON result from the Insights Query API """
 
         succeeded = False
         count_retries = 0
@@ -168,24 +208,23 @@ class NewRelicInsightsQueryAPI():
                 pass
         return response.json() if succeeded else []
 
-    def get_events(self, nrql, include={}):
-        """ execute the nrql and convert to an events array """
+    def events(self, nrql, include={}):
+        """ execute the nrql and convert to an events list """
+
+        _include = {}
+        _include.update(include)
 
         response = self.query(nrql)
         if not response:
             return []
 
-        metadata = o(response, 'metadata')
-        contents = metadata.get('contents', {})
-        results = response.get(
-            'results', response.get(
-                'facets', response.get(
-                    'timeSeries', []
-                )
-            )
-        )
+        del response['performanceStats']
+        print(json.dumps(response, sort_keys=True, indent=4))
 
-        # determine NRQL structure
+        metadata = response.get('metadata', {})
+        contents = metadata.get('contents', {})
+
+        # determine the NRQL structure
         has_compare_with = 'compareWith' in metadata
         has_facet = 'facet' in metadata or 'facet' in contents
         has_timeseries = 'timeSeries' in metadata or 'timeSeries' in contents
@@ -195,56 +234,96 @@ class NewRelicInsightsQueryAPI():
 
         # normalize the contents list
         if has_timeseries and (has_compare_with or has_facet):
-            contents = o(o(contents, 'timeSeries'), 'contents')
+            contents = contents['timeSeries']['contents']
         elif has_timeseries and not (has_compare_with or has_facet):
-            contents = o(o(metadata, 'timeSeries'), 'contents')
-        elif has_facet:
-            contents = o(contents, 'contents')
+            contents = metadata['timeSeries']['contents']
+        elif has_compare_with and has_facet:
+            contents = contents['contents']['contents']
+        elif has_compare_with or has_facet:
+            contents = contents['contents']
         else:
-            contents = o(metadata, 'contents')
+            contents = metadata['contents']
 
-        # build the header
-        facet = None
+        # get facets attribute names
         if has_facet:
             if has_compare_with:
-                facet = contents['facet']
+                facet = metadata['contents']['facet']
             else:
                 facet = metadata['facet']
+        else:
+            facet = None
 
+        # build the header and get the offset
         header = []
         if type(facet) is list:
             header.extend(facet)
+            offset = len(header)
         elif type(facet) is str:
             header.append(facet)
-        header.extend(get_results_names(contents))
+            offset = 1
+        else:
+            offset = 0
+        header.extend(get_results_header(contents))
 
-        # select the proper parsing function
-        fetch_data = get_none
+        # select the proper parsing function and parameters
         if has_single:
             fetch_data = get_single
-        if has_events:
+            results = response['results']
+            _include.update({
+                'timewindow': int((metadata['endTimeMillis'] - metadata['beginTimeMillis']) / 1000),
+                'timestamp': int(metadata['endTimeMillis'] / 1000)
+            })
+
+        elif has_events:
             fetch_data = get_events
-        elif has_compare_with and has_facet:
-            fetch_data = get_calc_with_compare_facet
-        elif has_compare_with and has_timeseries:
-            fetch_data = get_calc_with_compare_timeseries
+            results = response['results']
+
         elif has_compare_with:
-            fetch_data = get_calc_with_compare
-        elif has_facet and has_timeseries:
-            fetch_data = get_facets_timeseries
+            if has_facet:
+                fetch_data = get_calc_with_compare_facet
+                _include.update({
+                    'timewindow': int((metadata['endTimeMillis'] - metadata['beginTimeMillis']) / 1000),
+                    'timestamp': int(metadata['endTimeMillis'] / 1000),
+                    'timestamp_compare': int((metadata['beginTimeMillis'] - metadata['compareWith']) / 1000)
+                })
+            elif has_timeseries:
+                fetch_data = get_calc_with_compare_timeseries
+            else:
+                fetch_data = get_compare_with
+                _include.update({
+                    'timewindow': int((metadata['endTimeMillis'] - metadata['beginTimeMillis']) / 1000),
+                    'timestamp': int(metadata['endTimeMillis'] / 1000),
+                    'timestamp_compare': int((metadata['beginTimeMillis'] - metadata['compareWith']) / 1000)
+                })
+            results = {'current': response['current'], 'previous': response['previous']}
+
         elif has_facet:
-            fetch_data = get_facets
+            if has_timeseries:
+                fetch_data = get_facets_timeseries
+            else:
+                fetch_data = get_facets
+                _include.update({
+                    'timewindow': int((metadata['endTimeMillis'] - metadata['beginTimeMillis']) / 1000),
+                    'timestamp': int(metadata['endTimeMillis'] / 1000)
+                })
+            results = response['facets']
+
         elif has_timeseries:
             fetch_data = get_timeseries
+            results = response['timeSeries']
 
-        return fetch_data(results, header, include)
+        else:
+            fetch_data = get_empty
+            results = []
+
+        # parse the result JSON an return a list of event
+        return fetch_data(results, header, _include, offset)
 
 if __name__ == "__main__":
     new_relic_account_id = 2328917
     new_relic_insights_query_api_key = ''
     api = NewRelicInsightsQueryAPI(new_relic_account_id, new_relic_insights_query_api_key)
-    nrql = "select count(*), average(duration) as 'avg' from Transaction timeseries 5 hour since 1 months ago facet appName"
+    nrql = "select count(*), average(duration) from Transaction compare with 1 day ago facet appName"
     #events = api.query(nrql)
-    events = api.get_events(nrql, include={'eventType': 'Test'})
+    events = api.events(nrql, include={'eventType': 'Test'})
     print(json.dumps(events, sort_keys=True, indent=4))
-    print('done')
