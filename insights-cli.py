c34eeb325812c03f3b40344c78dc0b321f88535a
Paulo Monteiro <pauloesquilo@gmail.com>
2019-05-11 22:24:57 -0700

    query command complete

--- insights-cli.py
+++ insights-cli.py
@@ -1,13 +1,75 @@
-
+import csv
 import json
 import os
+import sys
 
 from insights_cli_view import get_cmdline_args
 from newrelic_query_api import NewRelicQueryAPI
 
 
-def do_query():
-    pass
+def abort(message):
+    """ abort the command """
+
+    print(message)
+    exit()
+
+def _open(filename: str = None, mode: str = 'r', *args, **kwargs):
+    """ file open method that can handle stdin and stdout """
+
+    if filename == '-':
+        if 'r' in mode:
+            stream = sys.stdin
+        else:
+            stream = sys.stdout
+        if 'b' in mode:
+            handler = stream.buffer
+        else:
+            handler = stream
+    else:
+        handler = open(filename, mode, *args, **kwargs)
+
+    return handler
+
+
+def do_query(**args):
+
+    query = args['query']
+    account_id = args['account_id']
+    query_api_key = args['query_api_key']
+    output_file = args['output_file']
+    output_format = args['output_format']
+
+    if not account_id:
+        account_id = os.getenv('NEW_RELIC_ACCOUNT_ID', '')
+    if not account_id:
+        abort('account id not provided and env NEW_RELIC_ACCOUNT_ID not set')
+
+    if not query_api_key:
+        query_api_key = os.getenv('NEW_RELIC_QUERY_API_KEY', '')
+    if not query_api_key:
+        abort('query api key not provided and env NEW_RELIC_QUERY_API_KEY not set')
+
+    if query == '-':
+        with _open(query) as f:
+            nrql = f.read()
+    else:
+        nrql = query
+
+    api = NewRelicQueryAPI(account_id, query_api_key)
+    events = api.events(nrql, include={'account_id': account_id})
+
+    if not output_file:
+        output_file = '-'
+
+    with _open(output_file, 'w') as f:
+        if output_format == 'json':
+            json.dump(events, f, sort_keys=True, indent=4)
+        else:
+            if len(events):
+                csv_writer = csv.DictWriter(f, fieldnames=events[0].keys())
+                csv_writer.writeheader()
+                for event in events:
+                    csv_writer.writerow(event)
 
 
 def do_batch_local():
@@ -26,20 +88,8 @@ def main():
     args, error = get_cmdline_args()
     if error:
         error()
-
-    elif args['command'] == 'query':
-        do_query()
-
-    elif args['command'] == 'batch-local':
-        do_batch_local()
-
-    elif args['command'] == 'batch-google':
-        do_batch_google()
-
-    elif args['command'] == 'batch-insights':
-        do_batch_insights()
-
-    print(args)
+    else:
+        globals()[args.command](**vars(args))
 
 if __name__ == "__main__":
     main()
--- insights_cli_view.py
+++ insights_cli_view.py
@@ -10,17 +10,21 @@ def get_cmdline_args():
     prepare_batch_google_parser(subparsers)
     prepare_batch_insights_parser(subparsers)
     
-    args = vars(parser.parse_args())
-    error = parser.print_help() if args['command'] == None else None
-    
+    args = parser.parse_args()
+    error = parser.print_help if args.command == None else None
+
     return args, error
 
 
 def prepare_query_parser(subparsers):
     query_parser = subparsers.add_parser('query')
-    query_parser.set_defaults(command='query')
-    query_parser.add_argument('-k', '--query-key',
+    query_parser.set_defaults(command='do_query')
+    query_parser.add_argument('-k', '--query-api-key',
         help='New Relic Insights query API key'
+    )
+    query_parser.add_argument('-a', '--account-id',
+        help='New Relic account ID',
+        type=int
     )
     query_parser.add_argument('-o', '--output-file',
         help='output file name'
@@ -37,16 +41,19 @@ def prepare_query_parser(subparsers):
 
 def prepare_batch_local_parser(subparsers):
     batch_local_parser = subparsers.add_parser('batch-local')
-    batch_local_parser.set_defaults(command='batch-local')
+    batch_local_parser.set_defaults(command='do_batch_local')
     batch_local_parser.add_argument('-v', '--vault-file',
-        help='New Relic vault file formatted as CSV [key_name,key_value]'
+        help='New Relic vault file formatted as CSV [key_name,key_value]',
+        type=argparse.FileType('r')
     )
     batch_local_parser.add_argument('-q', '--query-file',
         help='Local JSON input queries [{"name": "name", "query": "query"},...]',
+        type=argparse.FileType('r'),
         required=True
     )
     batch_local_parser.add_argument('-a', '--account-file',
         help='Local CSV input accounts [master_name,account_name,query_key]',
+        type=argparse.FileType('r'),
         required=True
     )
     batch_local_parser.add_argument('-o', '--output-folder',
@@ -62,12 +69,14 @@ def prepare_batch_local_parser(subparsers):
 
 def prepare_batch_google_parser(subparsers):
     batch_google_parser = subparsers.add_parser('batch-google')
-    batch_google_parser.set_defaults(command='batch-google')
+    batch_google_parser.set_defaults(command='do_batch_google')
     batch_google_parser.add_argument('-v', '--vault-file',
-        help='New Relic vault file formatted as CSV [key_name,key_value]'
+        help='New Relic vault file formatted as CSV [key_name,key_value]',
+        type=argparse.FileType('r')
     )
     batch_google_parser.add_argument('-q', '--query-file',
         help='Input queries list formatted as JSON [{name, query}, ...]',
+        type=argparse.FileType('r'),
         required=True
     )
     batch_google_parser.add_argument('-a', '--account-file-id',
@@ -80,25 +89,29 @@ def prepare_batch_google_parser(subparsers):
     )
     batch_google_parser.add_argument('-s', '--secret-file',
         help='Google secret file location',
+        type=argparse.FileType('r'),
         required=True
     )
 
 
 def prepare_batch_insights_parser(subparsers):
     batch_insights_parser = subparsers.add_parser('batch-insights')
-    batch_insights_parser.set_defaults(command='batch-insights')
+    batch_insights_parser.set_defaults(command='do_batch_insights')
     batch_insights_parser.add_argument('-v', '--vault-file',
-        help='New Relic vault file formatted as CSV [key_name,key_value]'
+        help='New Relic vault file formatted as CSV [key_name,key_value]',
+        type=argparse.FileType('r')
     )
     batch_insights_parser.add_argument('-q', '--query-file',
         help='Input queries list formatted as JSON',
+        type=argparse.FileType('r'),
         required=True
     )
     batch_insights_parser.add_argument('-a', '--account-file',
         help='Input accounts formatted as CSV [master_name,account_name,query_key]',
+        type=argparse.FileType('r'),
         required=True
     )
-    batch_insights_parser.add_argument('-i', '--insert-key',
+    batch_insights_parser.add_argument('-i', '--insert-api-key',
         help='New Relic Insights insert API key',
         required=True
     )
